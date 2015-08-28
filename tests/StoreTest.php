ac763b0318fd2c1e9ba566834ec58076838f078e
Jason Bethel <jlbethel@gmail.com>
2015-08-28 09:07:08 -0700

    write first getStoreName test

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -7,14 +7,25 @@
     require_once "src/Store.php";
     // require_once "src/Brand.php";
 
-    $server = 'mysql:host=localhost;dbname=registrar_test';
+    $server = 'mysql:host=localhost;dbname=shoes_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
     class StoreTest extends PHPUnit_Framework_TestCase
     {
+        function testGetStoreName()
+        {
+            //Arrange
+            $store_name = "Shoe World";
+            $test_store = new Store($store_name);
 
+            //Act
+            $result = $test_store->getName();
+
+            //Assert
+            $this->assertEquals($store_name, $result);
+        }
     }
 
 ?>
